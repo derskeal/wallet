26a2d43d09cb9dd9a478e345cca0e0c2b46cccc8
Derskeal <27731088+derskeal@users.noreply.github.com>
2022-05-11 21:20:19 +0100

    refactor env setup

--- src/config/envs/index.ts
+++ src/config/envs/index.ts
@@ -1,17 +1,10 @@
 import dotenv from 'dotenv';
 
-let env;
-
-if (process.env.NODE_ENV !== 'staging' && process.env.NODE_ENV !== 'production') {
-  env = dotenv.config().parsed;
-  if (!env) throw new Error('failed to parse env');
-} else {
-  env = process.env;
-}
+dotenv.config();
 
 export const envs = {
   ...process.env,
-  JWT_SECRET: env.JWT_SECRET || 'somesecret',
-  PAYSTACK_SECRET_KEY: env.PAYSTACK_SECRET_KEY,
+  JWT_SECRET: process.env.JWT_SECRET || 'somesecret',
+  PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
 };
 export const isProduction = process.env.NODE_ENV === 'production';
